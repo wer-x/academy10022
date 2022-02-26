414f4034a5922b03e953ba140d7331f2f7d05853
d.barukin <dmitrybarukin@gmail.com>
2022-02-26 15:50:37 +0300

    v1

--- academy10022/src/main/java/by/academy/lesson1/Application.java
+++ academy10022/src/main/java/by/academy/lesson1/Application.java
@@ -7,7 +7,7 @@ public class Application {
 		Cat cat2 = new Cat(1);
 		Cat cat3 = new Cat(2, "xex");
 		cat1.setAge(4);
-		cat1.setNickname("Жора");
+		cat1.setNickname("Кокожора");
 
 		cat1.grow(7);
 		String sAnswer = cat1.getAge() <= 10 ? "Кот еще жив" : "Кот " + cat1.getNickname() + " мертв:(";
